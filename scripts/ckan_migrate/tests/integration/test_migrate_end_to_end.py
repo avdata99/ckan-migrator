a301cc95a72b5b73f410527c2e17d63abdf060d8
German Favaro <germankay@gmail.com>
2025-08-25 15:35:32 -0300

    Andando

--- scripts/ckan_migrate/tests/integration/test_migrate_end_to_end.py
+++ scripts/ckan_migrate/tests/integration/test_migrate_end_to_end.py
@@ -8,23 +8,28 @@ import psycopg2.extras
 def _psql(pg, dbname=None, user="test", password="test"):
     host = "localhost"
     port = int(pg.get_exposed_port(5432))
-    if not dbname:
-        dbname = pg.get_connection_url().split("/")[-1]
     return PSQL(host=host, port=port, dbname=dbname, user=user, password=password)
 
+
 def test_full_migrate_users_groups_orgs(pg_old, pg_new):
-    old_db = _psql(pg_old)
-    new_db = _psql(pg_new)
+    # fixtures vienen como (container, dsn)
+    old_pg, old_dsn = pg_old
+    new_pg, new_dsn = pg_new
+
+    old_dbname = old_dsn.split("/")[-1]
+    new_dbname = new_dsn.split("/")[-1]
+
+    old_db = _psql(old_pg, dbname=old_dbname)
+    new_db = _psql(new_pg, dbname=new_dbname)
     assert old_db.connect() and new_db.connect()
 
     old_db.cursor = old_db.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     new_db.cursor = new_db.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
-    ulog = import_users(old_db, new_db)
-    glog = import_groups(old_db, new_db)
-    olog = import_organizations(old_db, new_db)
+    import_users(old_db, new_db)
+    import_groups(old_db, new_db)
+    import_organizations(old_db, new_db)
 
-    # commit y verificaciones
     new_db.conn.commit()
     new_db.cursor.execute('SELECT count(*) FROM "user"')
     assert new_db.cursor.fetchone()["count"] >= 2
